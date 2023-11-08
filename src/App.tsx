dde6e07d0e7eabaec7767cfd69d1e41b51c5d8ed
akmalnatha <akmalabhirama2002@gmail.com>
2023-11-08 19:30:22 +0700

    add color and font

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,26 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
+        {/* <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
+        Kontol
       </div>
       <h1 className='text-5xl font-black'>Vite + React</h1>
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)} className='text-red-600'>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">
+      </div> */}
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: {
+          primary: "#ED7D31",
+          secondary: "#F9B572",
+          ternary: "#F6F1EE",
+        },
+        blue: {
+          primary: "#363062",
+          secondary: "#6586C9",
+          ternary: "#91AFF9",
+        },
+        green: {
+          primary: "#137411",
+          secondary: "#76DD52",
+        },
+        red: {
+          primary: "#F14747"
+        },
+        mono: {
+          white: "#FFFFFF",
+          off_white: "#F7F7F7",
+          light_grey: "#A8A8A8",
+          grey: "#6B6B6B",
+        }
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 };
