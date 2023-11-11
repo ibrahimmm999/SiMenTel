fbe12b4dfdc7b098c67c9fbe6cbe67af28e7eee8
sulsul2 <18221110@std.stei.itb.ac.id>
2023-11-11 08:30:47 +0700

    fix : detail room

--- src/pages/dashboard/detailRoom/page.tsx
+++ src/pages/dashboard/detailRoom/page.tsx
@@ -12,10 +12,12 @@ function DetailRoom() {
           alt=""
           className="w-[560px] h-[390px] rounded-lg"
         />
-        <div className="flex flex-col">
-          <p className="text-[48px] font-semibold">Ruang Mawar</p>
-          <p className="text-[24px] font-semibold">Lantai 3</p>
-          <p className="text-[16px] font-normal mt-8">
+        <div className="flex flex-col justify-between">
+          <div className="flex flex-col gap-0">
+            <p className="text-[48px] font-semibold">Ruang Mawar</p>
+            <p className="text-[24px] font-semibold">Lantai 3</p>
+          </div>
+          <p className="text-[16px] font-normal">
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout. The point
             of using Lorem Ipsum is that it has a more-or-less normal
@@ -23,23 +25,25 @@ function DetailRoom() {
             here', making it look like readable English. Many desktop publishing
             packages and web page editors now
           </p>
-          <div className="w-full flex justify-between items-center mt-12">
-            <p className="text-[24px] font-bold">Price: Rp4.000.000,00</p>
-            <Button type={"button"} text="Edit Detail"/>
+          <div className="flex flex-col gap-5">
+            <div className="w-full flex justify-between items-center">
+              <p className="text-[24px] font-bold">Price: Rp4.000.000,00</p>
+              <Button type={"button"} text="Edit Detail" />
+            </div>
+            <hr className=" h-1 bg-black" />
           </div>
-          <hr className="mt-6 h-1 bg-black"/>
         </div>
       </div>
       <p className="text-[32px] font-medium mt-12 px-28">Facilities</p>
       <div className="flex gap-14 mt-8 flex-wrap px-28">
-        <Facility status={true} name={"Air Conditioner"}/>
-        <Facility status={true} name={"Air Conditioner"}/>
-        <Facility status={false} name={"Air Conditioner"}/>
-        <Facility status={true} name={"Air Conditioner"}/>
-        <Facility status={true} name={"Air Condi"}/>
-        <Facility status={true} name={"Air Condi"}/>
-        <Facility status={true} name={"Air Condi"}/>
-        <Facility status={true} name={"Air Conditioner"}/>
+        <Facility status={true} name={"Air Conditioner"} />
+        <Facility status={true} name={"Air Conditioner"} />
+        <Facility status={false} name={"Air Conditioner"} />
+        <Facility status={true} name={"Air Conditioner"} />
+        <Facility status={true} name={"Air Condi"} />
+        <Facility status={true} name={"Air Condi"} />
+        <Facility status={true} name={"Air Condi"} />
+        <Facility status={true} name={"Air Conditioner"} />
       </div>
     </div>
   );
