042411fe90847b3950024a9d913e217ef3811763
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-11-25 16:37:29 +0700

    feat: maintenance page, action

--- src/components/action.tsx
+++ src/components/action.tsx
@@ -3,19 +3,69 @@ import { BiSolidPencil } from "react-icons/bi";
 import { FaTrashCan } from "react-icons/fa6";
 import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
 
-function Action({ id, status }: { id: number; status: string }) {
+function Action({
+  id,
+  status,
+  onChangeEdit,
+  onChangeDelete,
+  onChangeAccept,
+  onChangeReject,
+}: {
+  id: string;
+  status: string;
+  onChangeEdit?: (x: string) => void;
+  onChangeDelete?: (x: string) => void;
+  onChangeAccept?: (x: string) => void;
+  onChangeReject?: (x: string) => void;
+}) {
   console.log(id);
   return (
     <>
       {status == "WAITING" ? (
         <div className="w-full flex justify-center gap-2">
-          <Button type={"button"} icon={<BiSolidPencil />} color="primary" />
-          <Button type={"button"} icon={<FaTrashCan />} color="red" />
+          <Button
+            type={"button"}
+            icon={<BiSolidPencil />}
+            color="primary"
+            onClick={() => {
+              if (onChangeEdit) {
+                onChangeEdit(id);
+              }
+            }}
+          />
+          <Button
+            type={"button"}
+            icon={<FaTrashCan />}
+            color="red"
+            onClick={() => {
+              if (onChangeDelete) {
+                onChangeDelete(id);
+              }
+            }}
+          />
         </div>
       ) : status == "REVIEW" ? (
         <div className="w-full flex justify-center gap-2">
-          <Button type={"button"} icon={<AiOutlineCheck />} color="green" />
-          <Button type={"button"} icon={<AiOutlineClose />} color="red" />
+          <Button
+            type={"button"}
+            icon={<AiOutlineCheck />}
+            color="green"
+            onClick={() => {
+              if (onChangeAccept) {
+                onChangeAccept(id);
+              }
+            }}
+          />
+          <Button
+            type={"button"}
+            icon={<AiOutlineClose />}
+            color="red"
+            onClick={() => {
+              if (onChangeReject) {
+                onChangeReject(id);
+              }
+            }}
+          />
         </div>
       ) : status == "FIXED" ? (
         <div className="w-full flex justify-center text-green-primary text-xl">
--- src/pages/maintenance/page.tsx
+++ src/pages/maintenance/page.tsx
@@ -9,7 +9,7 @@ import Datepicker from "../../components/datepicker";
 import Checkbox from "../../components/checkbox";
 import Dropdown from "../../components/dropdown";
 import Action from "../../components/action";
-import { useNavigate } from "react-router-dom";
+import { MdOutlineFileUpload } from "react-icons/md";
 import { FaFile } from "react-icons/fa6";
 import { toastError, toastSuccess } from "../../components/toast";
 import Textfield from "../../components/textfield";
@@ -97,7 +97,11 @@ function Maintenance() {
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
+  // const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [showEditPopUp, setShowEditPopUp] = useState<boolean>(false);
   const [maintenanceData, setMaintenanceData] = useState<Maintenance[]>([]);
+  const [editMaintenanceData, setEditMaintenanceData] =
+    useState<Maintenance | null>(null);
   const [roomData, setRoomData] = useState<Room[]>([]);
   const [userData, setUserData] = useState<User[]>([]);
   const [tanggal, setTanggal] = useState<Date | null>();
@@ -108,7 +112,6 @@ function Maintenance() {
   const [checkboxValues, setCheckboxValues] = useState<Record<string, boolean>>(
     listDetail.reduce((acc, detail) => ({ ...acc, [detail.value]: false }), {})
   );
-  const navigate = useNavigate();
   const [checkedColumns, setCheckedColumns] = useState<Record<string, boolean>>(
     kolomAdmin.reduce((acc, column) => ({ ...acc, [column]: true }), {})
   );
@@ -192,6 +195,72 @@ function Maintenance() {
     }
   };
 
+  const handleAcceptMaintenance = async (maintenanceId: number) => {
+    try {
+      setLoading(true);
+
+      const { data, error } = await supabase
+        .from("maintenances")
+        .update({
+          status: "FIXED",
+        })
+        .eq("id", maintenanceId);
+
+      if (error) {
+        console.error("Error accepting maintenance:", error);
+      } else {
+        console.log("Maintenance accepted successfully:", data);
+      }
+
+      setLoading(false);
+      fetchData();
+    } catch (error) {
+      console.error("Error accepting maintenance:", error);
+      setLoading(false);
+    }
+  };
+  const handleRejecttMaintenance = async (
+    maintenanceId: number,
+    evidenceUrl: string
+  ) => {
+    try {
+      setLoading(true);
+
+      // Hapus file dari penyimpanan Supabase
+      const fileName = evidenceUrl.split("/").pop(); // Mendapatkan nama file dari URL
+      const { data: dataFile, error: errorDelete } = await supabase.storage
+        .from("evidence")
+        .remove([`public/${fileName}`]);
+
+      if (errorDelete) {
+        console.error("Error deleting file from storage:", errorDelete);
+      } else {
+        console.log("File deleted successfully:", dataFile);
+      }
+
+      // Setelah menghapus file, update status maintenance
+      const { data, error } = await supabase
+        .from("maintenances")
+        .update({
+          status: "WAITING",
+          evidence_url: null,
+        })
+        .eq("id", maintenanceId);
+
+      if (error) {
+        console.error("Error rejecting maintenance:", error);
+      } else {
+        console.log("Maintenance rejected successfully:", data);
+      }
+
+      setLoading(false);
+      fetchData();
+    } catch (error) {
+      console.error("Error rejecting maintenance:", error);
+      setLoading(false);
+    }
+  };
+
   const handleDeleteMaintenance = async (maintenanceId: number) => {
     try {
       setLoading(true);
@@ -212,6 +281,64 @@ function Maintenance() {
       fetchData();
     } catch (error) {
       console.error("Error deleting maintenance data:", error);
+      setLoading(false);
+    }
+  };
+
+  const handleFileUpload = async (maintenanceId: number) => {
+    try {
+      // Open file explorer and get the selected file
+      const input = document.createElement("input");
+      input.type = "file";
+      input.accept = ".pdf"; // Add any specific file formats you want to accept
+      input.click();
+
+      input.addEventListener("change", async (e) => {
+        const selectedFile = (e.target as HTMLInputElement).files?.[0];
+
+        if (selectedFile) {
+          toastSuccess(selectedFile?.name);
+          setLoading(true);
+
+          const { data: fileData, error: fileError } = await supabase.storage
+            .from("evidence")
+            .upload(`public/${selectedFile.name}`, selectedFile, {
+              cacheControl: "3600",
+              upsert: false,
+            });
+
+          if (fileError) {
+            console.error("Error uploading file:", fileError);
+            toastError("Error uploading file");
+          } else {
+            console.log("File uploaded successfully:", fileData);
+            toastSuccess("File uploaded successfully");
+
+            const { data } = supabase.storage
+              .from("evidence")
+              .getPublicUrl(`public/${selectedFile.name}`);
+
+            const { data: updateData, error: updateError } = await supabase
+              .from("maintenances")
+              .update({ evidence_url: data.publicUrl, status: "REVIEW" })
+              .eq("id", maintenanceId);
+
+            if (updateError) {
+              console.error("Error updating maintenance data:", updateError);
+              toastError("Error updating maintenance data");
+            } else {
+              console.log("Maintenance data updated successfully:", updateData);
+
+              // Fetch updated data
+              fetchData();
+            }
+          }
+
+          setLoading(false);
+        }
+      });
+    } catch (error) {
+      console.error("Error handling file upload:", error);
       setLoading(false);
     }
   };
@@ -243,7 +370,18 @@ function Maintenance() {
         onClick={() => handleOnClickLaporan(item.evidence_url)}
         icon={<FaFile />}
       />,
-      <Action id={item.id} status={item.status} />,
+      <Action
+        id={item.id.toString()}
+        status={item.status}
+        onChangeEdit={(id) => {
+          handleEditMaintenance(parseInt(id));
+        }}
+        onChangeDelete={(id) => handleDeleteMaintenance(parseInt(id))}
+        onChangeAccept={(id) => handleAcceptMaintenance(parseInt(id))}
+        onChangeReject={(id) =>
+          handleRejecttMaintenance(parseInt(id), item.evidence_url)
+        }
+      />,
     ].filter((_, i) => filteredColumns.includes(kolomAdmin[i]));
   });
 
@@ -261,10 +399,10 @@ function Maintenance() {
       item.room?.floor || "",
       item.detail,
       <Button
-        disable={item.status == "WAITING"}
-        type={"button"}
-        onClick={() => handleOnClickLaporan(item.evidence_url)}
-        icon={<FaFile />}
+        disable={item.status === "FIXED"}
+        type="button"
+        onClick={() => handleFileUpload(item.id)}
+        icon={<MdOutlineFileUpload />}
       />,
     ].filter((_, i) => filteredColumns.includes(kolomStaff[i]));
   });
@@ -288,7 +426,90 @@ function Maintenance() {
   };
 
   const handleOnClickLaporan = (url: string) => {
-    window.location.replace(url);
+    window.open(url, "_blank");
+  };
+
+  const handleEditMaintenance = (maintenanceId: number) => {
+    const maintenanceToEdit = maintenanceData.find(
+      (maintenance) => maintenance.id === maintenanceId
+    );
+    if (maintenanceToEdit) {
+      // Extract values from maintenanceToEdit and set state variables
+      const { assign_time, user, room, detail } = maintenanceToEdit;
+      setTanggal(assign_time ? new Date(assign_time) : null);
+      setStaff(user?.name || "");
+      setRuangan(room?.name || "");
+
+      // Create an object to represent the checkbox values
+      const newCheckboxValues: Record<string, boolean> = {};
+      listDetail.forEach((detailOption) => {
+        newCheckboxValues[detailOption.value] = detail.includes(
+          detailOption.value
+        );
+      });
+      setCheckboxValues(newCheckboxValues);
+
+      setEditMaintenanceData(maintenanceToEdit);
+      setShowEditPopUp(true);
+    } else {
+      console.error(`Maintenance with ID ${maintenanceId} not found.`);
+    }
+  };
+
+  const handleSaveEditMaintenance = async () => {
+    try {
+      setLoading(true);
+
+      if (editMaintenanceData) {
+        const existingMaintenance = await supabase
+          .from("maintenances")
+          .select("*")
+          .eq("id", editMaintenanceData.id)
+          .single();
+
+        if (existingMaintenance.data) {
+          const updatedMaintenance = {
+            assign_time: tanggal?.toISOString() || "",
+            work_time: null,
+            status: "WAITING",
+            evidence_url: "-",
+            room_id: roomData.find((room) => room.name === ruangan)?.id || 1,
+            detail: listDetail
+              .filter((detail) => checkboxValues[detail.value])
+              .map((selectedDetail) => selectedDetail.value)
+              .join(", "),
+            user_id: userData.find((user) => user.name === staff)?.id || "",
+          };
+
+          const { data: updateData, error: updateError } = await supabase
+            .from("maintenances")
+            .update(updatedMaintenance)
+            .eq("id", editMaintenanceData.id);
+
+          if (updateError) {
+            toastError("Error updating maintenance data");
+            console.error("Error updating maintenance data:", updateError);
+          } else {
+            toastSuccess("Data updated successfully");
+            console.log(updateData);
+          }
+        } else {
+          console.log("data not found");
+        }
+
+        fetchData();
+        setLoading(false);
+        setShowEditPopUp(false);
+      } else {
+        console.error("No maintenance data to edit.");
+        setLoading(false);
+      }
+    } catch (error) {
+      toastError(error as string);
+      fetchData();
+      setLoading(false);
+      setShowEditPopUp(false);
+    }
   };
 
   // paginate
@@ -372,6 +593,88 @@ function Maintenance() {
                 color="green"
                 text="Tambah Data"
                 onClick={handleAddMaintenance}
+              />
+            </div>
+          </div>
+        }
+      ></Modal>
+      <Modal
+        visible={showEditPopUp}
+        onClose={() => setShowEditPopUp(!showEditPopUp)}
+        children={
+          <div>
+            <h1 className=" text-[40px] text-[#4D4C7D] font-bold mb-5">
+              Edit Maintenance
+            </h1>
+            <div className="flex mb-7 w-full gap-10">
+              <div className="w-1/2">
+                <p className="mb-[8px] text-[16px] text-[#4D4C7D] font-semibold">
+                  Tanggal
+                </p>
+                <Datepicker
+                  defaultValue={tanggal || null}
+                  text="Masukkan Tanggal"
+                  required
+                  onChange={(val: Date) => setTanggal(val)}
+                />
+              </div>
+              <div className="w-1/2">
+                <p className="mb-[8px] text-[16px] text-[#4D4C7D] font-semibold">
+                  Staff
+                </p>
+                <Dropdown
+                  value={staffOptions.find((option) => option.value === staff)}
+                  required
+                  placeholder="Nama Staff"
+                  options={staffOptions}
+                  onChange={(e) => setStaff(e?.value!)}
+                />
+              </div>
+            </div>
+            <div className="flex w-full gap-10 mb-7">
+              <div className="w-[50%]">
+                <p className="mb-[8px] text-[16px] text-[#4D4C7D] font-semibold">
+                  Ruangan
+                </p>
+                <Dropdown
+                  value={roomOptions.find((option) => option.value === ruangan)}
+                  placeholder="Nama Ruangan"
+                  options={roomOptions}
+                  onChange={(e) => setRuangan(e?.value!)}
+                />
+              </div>
+              <div className="w-1/2">
+                <p className="mb-[8px] text-[16px] text-[#4D4C7D] font-semibold">
+                  Detail Maintenance
+                </p>
+                <div className="flex mt-6">
+                  {listDetail.map((detail) => (
+                    <Checkbox
+                      key={detail.value}
+                      label={detail.label}
+                      id={detail.value}
+                      checked={checkboxValues[detail.value]}
+                      onChange={() => handleCheckboxChange(detail.value)}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-end mt-16 gap-4">
+              <Button
+                type="button"
+                color="red"
+                text="Batalkan"
+                isLoading={loading}
+                onClick={() => setShowEditPopUp(false)}
+              />
+              <Button
+                isLoading={loading}
+                type="submit"
+                color="green"
+                text="Edit Data"
+                onClick={handleSaveEditMaintenance}
               />
             </div>
           </div>
